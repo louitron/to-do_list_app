6aeb6c149160478a0d358f170a1c2d7757b9379f
louitron <luisgarcia1437@gmail.com>
2023-03-14 00:56:34 +0000

    continuing refactoring using flux and zustan

--- src/components/services/useToDoList.js
+++ src/components/services/useToDoList.js
@@ -1,19 +1,26 @@
 import { create } from "zustand";
 
 export const useTodoList = create((set) => ({
-  task: "",
-  list: [],
+  task: "", //Use this in place of inputValue
+  list: [], //Use this in place of tasks
 
   changeTask: (value) => {
     set({ task: value });
   },
 
-  clearTask: () => {
-    set({ task: "" });
+  addTask: () => {
+    set((store) => ({
+      list: [...store.list, { task: store.task, done: false }],
+      task: "",
+    }));
   },
 
-  addTask: (task) => {
-    set((store) => ({ list: [...store.list, { task, done: false }] }));
+  deleteTask: (index) => {
+    set((store) => {
+      const newTasks = [...store.list];
+      newTasks.splice(index, 1);
+      list: newTasks;
+    });
   },
 
   completeTask: (index) => {
