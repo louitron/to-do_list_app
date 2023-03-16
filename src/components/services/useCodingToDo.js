80f6b938b57cf5e14c4b487a8b1041b89843e575
louitron <luisgarcia1437@gmail.com>
2023-03-16 12:59:49 +0000

    added 'persist' state storage for list components

--- src/components/services/useCodingToDo.js
+++ src/components/services/useCodingToDo.js
@@ -1,44 +1,53 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useCodingToDo = create((set, get) => ({
-  tasks: [],
-  inputValue: "",
+const useCodingToDo = create(
+  persist(
+    (set, get) => ({
+      tasks: [],
+      inputValue: "",
 
-  setInputValue: (value) => set({ inputValue: value }),
+      setInputValue: (value) => set({ inputValue: value }),
 
-  addTask: () => {
-    const { inputValue, tasks } = get();
-    if (inputValue) {
-      set({
-        tasks: [...tasks, { task: inputValue, done: false }],
-        inputValue: "",
-      });
+      addTask: () => {
+        const { inputValue, tasks } = get();
+        if (inputValue) {
+          set({
+            tasks: [...tasks, { task: inputValue, done: false }],
+            inputValue: "",
+          });
+        }
+      },
+
+      deleteTask: (index) => {
+        const { tasks } = get();
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        set({ tasks: newTasks });
+      },
+
+      completedTask: (index) => {
+        const { tasks } = get();
+        const newTasks = [...tasks];
+        newTasks[index].done = !newTasks[index].done;
+        set({ tasks: newTasks });
+      },
+
+      getIncompleteTasks: (index) => {
+        const { tasks } = get();
+        return tasks.filter((task) => !task.done);
+      },
+      clearAllTasks: () => {
+        const { tasks } = get();
+        const deletedTasks = [];
+        set({ tasks: deletedTasks });
+      },
+    }),
+    {
+      name: "coding-todo-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
     }
-  },
-
-  deleteTask: (index) => {
-    const { tasks } = get();
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    set({ tasks: newTasks });
-  },
-
-  completedTask: (index) => {
-    const { tasks } = get();
-    const newTasks = [...tasks];
-    newTasks[index].done = !newTasks[index].done;
-    set({ tasks: newTasks });
-  },
-
-  getIncompleteTasks: (index) => {
-    const { tasks } = get();
-    return tasks.filter((task) => !task.done);
-  },
-  clearAllTasks: () => {
-    const { tasks } = get();
-    const deletedTasks = [];
-    set({ tasks: deletedTasks });
-  },
-}));
+  )
+);
 
 export default useCodingToDo;
--- src/components/services/useShoppingList.js
+++ src/components/services/useShoppingList.js
@@ -1,43 +1,52 @@
-import { create} from "zustand";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useShoppingList = create((set, get) => ({
-    items: [],
-    inputValue: "",
+const useShoppingList = create(
+  persist(
+    (set, get) => ({
+      items: [],
+      inputValue: "",
 
-    setInputValue: (value) => set({inputValue: value}),
+      setInputValue: (value) => set({ inputValue: value }),
 
-    addItem: () => {
-       const {inputValue, items} = get();
-       set({items: [...items, {task: inputValue, done: false}],
-       inputValue: ""})
-    },
+      addItem: () => {
+        const { inputValue, items } = get();
+        set({
+          items: [...items, { task: inputValue, done: false }],
+          inputValue: "",
+        });
+      },
 
-    deleteItem: (index) => {
+      deleteItem: (index) => {
         const { items } = get();
         const newItems = [...items];
         newItems.splice(index, 1);
-        set({items: newItems});
-    },
+        set({ items: newItems });
+      },
 
-    completedPurchase: (index) => {
+      completedPurchase: (index) => {
         const { items } = get();
         const newItems = [...items];
-        newItems[index].done = !newItems[index].done
+        newItems[index].done = !newItems[index].done;
         set({ items: newItems });
-    },
+      },
 
-    getIncompletePurchase: () => {
+      getIncompletePurchase: () => {
         const { items } = get();
         return items.filter((item) => !item.done);
-    },
+      },
 
-    clearAllItems: () => {
+      clearAllItems: () => {
         const { items } = get();
         const deletedItems = [];
         set({ items: deletedItems });
-      }
-
-
-}));
+      },
+    }),
+    {
+      name: "shopping-list-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage1), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
 
 export default useShoppingList;
